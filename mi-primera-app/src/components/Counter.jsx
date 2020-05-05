0c47e6fa961950b14c18464db63a9510094a4a62
mrboggii <60938259+mrboggii@users.noreply.github.com>
2020-05-05 13:47:44 +0200

    shit still working

--- mi-primera-app/src/components/Counter.jsx
+++ mi-primera-app/src/components/Counter.jsx
@@ -4,26 +4,33 @@ export default class Counter extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            count: 0,
-            isRed: false
+            count: this.props.inicial,
+            isRed: false,
+            movies:[{},{},{}]
         }
     }
     componentDidMount(){
-            this.setState({count:this.props.inicial});
+        console.log('componente montado',this.props.inicial)
     }
-    // componentDidUpdate(){
-    //     console.log(this.state.count)
-    // }
+    componentDidUpdate(){
+        console.log(this.state.count)
+    }
     increment = () => {
         if(this.state.count>15){
-            return this.componentDidMount();
+            return  this.setState({count:this.props.inicial});
         }
         this.setState({ count: this.state.count + 1, isRed: !this.state.isRed })
     }
+    deleteMovie = (movie_id)=>{
+        const filteredMovies =this.state.movies.filter(movie=>movie.id !== movie_id);
+        this.setState({movies:filteredMovies})
+    }
     render() {
         return (
-            <div onClick={this.increment} style={{ color: this.state.isRed ? 'red' : 'white' }}>
+            <div onClick={this.increment} 
+            style={{ color: this.state.isRed ? 'red' : 'white' }}>
                 {this.state.count}
+                <button onClick={()=>deleteMovie(movie_id)}>Eliminar Película</button>
             </div>
         )
     }
